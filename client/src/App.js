a79146734f3abcb00fd1735188d6e81675a4f186
Petko Kostadinov <petkoxray@gmail.com>
2018-05-28 12:56:22 +0300

    baisc layout with router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
+import HomePage from './components/shared/HomePage';
+import Footer from './components/shared/Footer';
+import Navbar from './components/shared/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={HomePage} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
